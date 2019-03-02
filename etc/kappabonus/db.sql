4a2d1961da4c837886fb9a64215df5864ba08e6e
Nikita Sychev <root@nsychev.ru>
2019-03-02 23:44:14 +0300

    fix sql template again

--- etc/kappabonus/db.sql
+++ etc/kappabonus/db.sql
@@ -1,26 +1,26 @@
-create database `kappabonus`;
+create database `{team}`;
 
-create table `user`(
+create table `{team}`.`user`(
     `username` varchar(40) not null,
     `password` char(82) not null,
-    `balance` int default 0,
+    `balance` bigint default 0,
     `vip` tinyint default 0,
     `posted_flags` int default 0,
     primary key(`username`)
 );
 
-create table `kappa`(
+create table `{team}`.`kappa`(
     `id` int not null auto_increment,
     `flag` char(32) not null,
-    `cost` int not null,
+    `cost` bigint not null,
     `username` varchar(40) not null,
     primary key(`id`)
 );
 
-create table `lcbc`(
+create table `{team}`.`lcbc`(
     `id` int not null auto_increment,
     `flag` char(32) not null,
-    `cost` int not null,
+    `cost` bigint not null,
     `username` varchar(40) not null,
     primary key(`id`)
 );
